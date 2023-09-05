43f84abe80f77715ddc8f4205d4918c01d104123
tanveerisonline <tanveerisonline@gmail.com>
2023-09-05 14:09:21 +0530

    basic routing setup done

--- i_applied_frontend/src/App.jsx
+++ i_applied_frontend/src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <h1>I-Applied App</h1>;
-}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>About Page</h1>
+      </div>
+    ),
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
